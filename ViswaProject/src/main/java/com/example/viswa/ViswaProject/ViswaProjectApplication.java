9faf1382e8da8d0293a7ef8b903fdf33b48a05cb
sujith <sujithreddy440@gmail.com>
2020-03-05 20:45:57 +0530

    haritha project

---
+++ ViswaProject/src/main/java/com/example/viswa/ViswaProject/ViswaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.viswa.ViswaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViswaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViswaProjectApplication.class, args);
+	}
+
+}
---
+++ ViswaProject/src/test/java/com/example/viswa/ViswaProject/ViswaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viswa.ViswaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViswaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
